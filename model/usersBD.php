763e4892dc69979a6d8d97c48aaaeb1090da6770
attar <attar@PC-Fuji-AxH>
2016-02-24 12:33:01 +0100

    -Modif de connectBD : require_once probleme de quote et lien fichier php

--- model/usersBD.php
+++ model/usersBD.php
@@ -5,22 +5,24 @@
  */
 
 	function connectUserBD($name, $password){
-		require_once(./control/connectBD.php);
+		require_once('./connectBD.php');
 		$bdd = connectBD();
 		$req = $bdd->prepare('SELECT * FROM users WHERE name = ? AND password = ?');
-		if ($req-> execute(array($name, $password)))
-			return true ;
-		return false ;
+		if ($req->execute(array($name, $password))) {
+        return true;
+    }
+    return false ;
 	}
 	
 	function signUpBD($name, $password) {
-		require_once(./control/connectBD.php);
+		require_once('./connectBD.php');
 		$bdd = connectBD();
 		$req = $bdd->prepare('INSERT INTO users VALUES (:username, :password)');
 		$req-> execute(array('username' => $name, 'password' => $password));
-		if (!isnull($req))
-			return true ;
-		return false ;
+		if (!isnull($req)) {
+        return true;
+    }
+    return false ;
 	}
 
 
