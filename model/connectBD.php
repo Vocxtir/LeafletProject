6588466273072fbe4c36d27416c44be9c5dfb8b8
VictorR <rahman.victor@outlook.fr>
2016-03-02 18:06:46 +0100

    Correcting db connection

--- model/connectBD.php
+++ model/connectBD.php
@@ -7,7 +7,11 @@
 
 	function connectBD(){
 		try{
-			$bdd = new PDO('mysql:host=localhost;dbname=dut2_rahman;charset=utf8', 'rahman', 'rahman');
+			$bdd = new PDO('mysql:host=localhost:8080; port=8080; dbname=leafletproject;
+				charset=utf8', 
+				'root', 
+				'');
+			
 			return $bdd ;
 		} catch (Exception $e){
 				die('Erreur : ' . $e->getMessage());
